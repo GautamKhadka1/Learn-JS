d1691bc6052aa09d6fdf83ab11c7de06881e7b5a
Gautam <gautamkhadka9863@gmail.com>
2024-03-27 12:33:45 +0545

    day3 of js learning

---
+++ day3.js
@@ -0,0 +1,11 @@
+//type of data type
+var x= "Gautam";//string
+let a= 15.50; //number
+let b= true; //boolean
+const Animal = {Color: "black", Type: "Dog"};//object  
+const y= ["Gautam", "Khadka"]; //array
+
+
+//Type of Operation
+let c =typeof "hello world";
+alert(c);

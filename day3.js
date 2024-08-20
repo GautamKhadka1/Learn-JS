5328d43781424e3d8214c7b22b3cd4beab8dc992
GautamKhadka1 <163504407+GautamKhadka1@users.noreply.github.com>
2024-08-20 21:46:18 +0545

    Day4

--- day3.js
+++ day3.js
@@ -1,49 +1,27 @@
-//type of data type
-var x= "Gautam";//string
-let a= 15.50; //number
-let b= true; //boolean
-const Animal = {Color: "black", Type: "Dog"};//object  
-const y= ["Gautam", "Khadka"]; //array
+//event Listener
+// In this project when your mouse is over button it will disappear & reappear 3 times and display message at the end when clicked. 
+let count =0;
+let x = Math.random()*10;
+console.log(x);
 
-
-//Type of Operation
-let c =typeof "hello world";
-document.write(c + "<br>");
-//Declaring Function 
-let d = Sum(5,6);
-
-function Sum(x,y) {
-    return x+y;
+const button = document.querySelector("#clickme");
+button.addEventListener("mouseover", e => {
+    count++;
+    if(count<x)
+    {
+    disappear();
+    }
+});
+function disappear()
+{
+ button.style.display ="none";
+ setTimeout(() => {
+    button.style.display ="block";
+ }, 700);
 }
-document.write("\nThe sum of is "+ d+"<br>");
-
-//Function Expression which means function can be assigned to variable as well as can be used in expressions aswell
-
-var e = function (x,y) { return x*y; };//anonymous function
-var z = e(5,67);
-document.write("\nThe multiplication of 5 & 67 is "+ z +"<br>");
-
-//hoisting mean calling function before they are declared 
-
-
-//Object 
-//declaring function inside of an object
-
-const Student={
-    firstName: "Gautam",
-    lastName: "Khadka",
-    age: 15,
-    address: "Bangalore",
-    fullName: function(){ return this.firstName+""+this.lastName;}
-};
-document.write(Student.fullName()+ "<br>");
-
-//Concept of NaN Not A Number which means that it is not a number eg below
-var x= "gautam";
-document.write(x/10 + "<br>");
-
-//but if the string is number then it will compute as numeric value
-var x= "100";
-document.write(x/10);
-
-
+function Display()
+{
+    const display = document.getElementById("Display");
+    button.style.display ="none";
+    display.innerText = "Thank you for clicking."
+}

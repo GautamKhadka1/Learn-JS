a730f02ef8fbf0a72234d406d0401bd2a2c30a1f
GautamKhadka1 <163504407+GautamKhadka1@users.noreply.github.com>
2024-08-23 10:29:02 +0545

    Office change

---
+++ Js1.js
@@ -0,0 +1,18 @@
+//this is practice of dom manipulation 
+// let a = window;
+// console.log(a)
+
+//taking input using prompt
+let name;
+name= prompt("Enter Your Name:");
+console.log(name);
+
+// Using alert
+alert("Hello "+ name + " do you want to continue....");
+
+//using confirm
+let bool = confirm("Are You Sure?");
+console.log(bool);
+
+//Dom Mnupulation document is Html file
+

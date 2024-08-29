bba9f31dc07cf151fb21f67ad1544828b64abe5e
GautamKhadka1 <163504407+GautamKhadka1@users.noreply.github.com>
2024-08-29 22:54:36 +0545

    Day10 Home

---
+++ Day10-CallBack.js
@@ -0,0 +1,59 @@
+function calculate(x, y, callback)//this is asynchronous callback 
+{
+    setTimeout(function(){
+        console.log("Entered Values are "+x+" and "+y);
+        callback(x,y);
+
+    }, 1000)
+}
+
+function sum(x,y)
+{
+    let z = x+y;
+    console.log("The sum of these two number is " + z);
+}
+function multiply(x,y)
+{
+    let z = x*y;
+    console.log("The multiple of these two number is "+ z);
+}
+calculate(5,6,sum);
+calculate(10,6,multiply);
+
+//real life example
+function getUserData(uid, callback)
+{
+    setTimeout(function(){
+        const data = {
+            1:{id:1, name:"Gautam"},
+            2:{id:2, name:"Hari"},
+            3:{id:3, name:"Shyam"}
+        };
+        const user = data[uid];
+        if(user)
+        {
+            callback(null, user);
+
+        }
+        else
+        {
+            callback("User not found.", null);
+        }
+
+    }, 1000);
+}
+
+function handleUser(error, user)
+{
+    if(error)
+    {
+        console.log("Error: " + error);
+    }
+    else
+    {
+        console.log(user);
+    }
+}
+
+getUserData(2, handleUser);
+getUserData(4, handleUser);

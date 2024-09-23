3a6ee881799b004c33889746008fbc934154fcf9
GautamKhadka1 <163504407+GautamKhadka1@users.noreply.github.com>
2024-09-23 16:24:23 +0545

    feat:Weather Icon added

--- weather_app.js
+++ weather_app.js
@@ -8,11 +8,57 @@ const search = document.querySelector("#search");
 
 
 async function checkweather(x){
-    const api_key = "e2bec2268c4e7ced754434d267736ea6"
-;
+    const api_key = "e2bec2268c4e7ced754434d267736ea6";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${x}&appid=${api_key}`;
     const weather_data = await fetch(`${url}`).then(response=> response.json());
     console.log(weather_data);
+    let t = weather_data.main.temp - 273;
+    temp.innerHTML = t.toFixed(1) + "°C";
+    let a = weather_data.weather[0].description
+    weather_text.innerHTML = a;
+    humidity.innerHTML = weather_data.main.humidity + "%";
+    wind.innerHTML = weather_data.wind.speed + "Km/h";
+    weather_icon.src= icon(a);
+    document.querySelector("#city").innerHTML= x;
+    
+}
+function icon(i){
+    switch (i){
+    case "clear sky":
+        return "sunny.png";
+        break;
+    case "few clouds":
+        return "partial.png";
+        break;
+    case "scattered clouds":
+        return "cloudy.png";
+        break;
+    case "broken clouds":
+        return "cloudy.png";
+        break;
+    case "shower rain":
+        return "rainy.png";
+        break;
+    case "rain":
+        return "rainy.png";
+        break;
+    case "thunderstorm":
+        return "thunder.png";
+        break;
+    case "snow":
+        return "snow.png";
+        break;
+    case "mist":
+        return "humid.png";
+        break;
+    case "overcast clouds":
+        return "cloudy.png";
+        break;
+    case "light rain":
+        return "rainy.png";
+        break;
+    }
+        
 }
 search.addEventListener('click',()=>{
     checkweather(input.value);
